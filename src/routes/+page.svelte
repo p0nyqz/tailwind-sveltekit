5ff7200be33b539b262f6015a7bd41ce8a5d6854
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-04-25 14:32:17 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<table class="border-collapse border border-slate-500 ...">
+	<thead>
+		<tr>
+			<th class="border border-slate-600 ...">State</th>
+			<th class="border border-slate-600 ...">City</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="border border-slate-700 ...">Indiana</td>
+			<td class="border border-slate-700 ...">Indianapolis</td>
+		</tr>
+		<tr>
+			<td class="border border-slate-700 ...">Ohio</td>
+			<td class="border border-slate-700 ...">Columbus</td>
+		</tr>
+		<tr>
+			<td class="border border-slate-700 ...">Michigan</td>
+			<td class="border border-slate-700 ...">Detroit</td>
+		</tr>
+	</tbody>
+</table>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
