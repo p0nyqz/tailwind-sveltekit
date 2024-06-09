4f314559f5276b15ca10916dba77aa03a224f77a
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-06-09 23:59:45 +0200

    feat: dashboard layout

---
+++ src/routes/(dashboard)/+layout.svelte
@@ -0,0 +1,18 @@
+<div class="grid grid-cols-12 gap-x-16 bg-whisper min-h-screen">
+  <header class="col-span-3 bg-daisyBush">
+    <div>
+      <a href="/invoices"><img src="" alt="" /></a>
+    </div>
+  <nav>
+    <ul>
+      <li><a href="/invoices">Invoices</a></li>
+      <li><a href="/clients">Clients</a></li>
+      <li><a href="#">Settings</a></li>
+      <li><a href="#">Logout</a></li>
+    </ul>
+  </nav>
+  </header>
+<main>
+  <slot></slot>
+</main>
+</div>
