4b9fcff204e0ab3796767bbed57698a7ef5e03b1
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-06-11 23:51:11 +0200

    copm: Navbar, styling

--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -1,27 +1,8 @@
-<div class="grid grid-cols-12 gap-x-16 bg-whisper min-h-screen">
-  <header class="col-span-3 bg-daisyBush text-center">
-    <div>
-      <a href="/invoices"><img src="/images/logo.svg" alt="The Dollar Holler" class="mx-auto" /></a>
-    </div>
-  <nav>
-    <ul class="list-none text-2xl font-bold font-sansSerif">
-      <li><a href="/invoices">Invoices</a></li>
-      <li><a href="/clients">Clients</a></li>
-      <li><a href="#">Settings</a></li>
-      <li><a href="#">Logout</a></li>
-    </ul>
-  </nav>
-  </header>
-<main class="col-span-8 pt-10">
-  <slot></slot>
-</main>
-</div>
+<script lang="ts">
+  import Navbar from "$lib/components/Navbar.svelte";
+</script>
 
-<style lang="postcss">
-  nav ul li {
-    @apply mb-6;
-  }
-  nav ul li a{
-    @apply font-bold text-white hover:text-goldenFizz;
-  }
-</style>
+<div class="grid grid-cols-12 gap-x-16 bg-whisper min-h-screen">
+  <Navbar />
+  <main class="col-span-8 pt-10"><slot></slot></main>
+</div>
